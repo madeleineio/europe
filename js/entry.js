1fd010baaa071008b051c83d56edd4b71eb83d94
nicolas mondon <nicolasmondon@gmail.com>
2014-10-28 22:15:34 +0100

    setup

---
+++ js/entry.js
@@ -0,0 +1,8 @@
+/**
+ * Created by nicolasmondon on 28/10/2014.
+ */
+
+'use strict';
+
+var d3 = require('../bower_components/d3/d3.js')
+    $ = require('../bower_components/jquery/dist/jquery.js');
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    entry: {
+        main: './js/entry.js'
+    },
+    output: {
+        path: 'public/js',
+        filename: '[name].js'
+    }
+};
