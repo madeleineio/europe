ed8a68eb43cbde661f047e88e1245febfc7db31a
nmondon <nmondon@lefigaro.fr>
2014-10-29 13:14:06 +0100

    retrieve datas

--- js/entry.js
+++ js/entry.js
@@ -4,5 +4,22 @@
 
 'use strict';
 
-var d3 = require('../bower_components/d3/d3.js')
-    $ = require('../bower_components/jquery/dist/jquery.js');
+// modules
+var d3 = require('../bower_components/d3/d3.js'),
+    $ = require('../bower_components/jquery/dist/jquery.js'),
+    Q = require('../node_modules/q/q.js');
+
+// vars
+var countries,
+    countriyDatas,
+    promiseGeojson = Q.Promise(function(resolve){
+        d3.json('public/data/countries.geo.json', resolve);
+    }),
+    promiseData = Q.Promise(function(resolve){
+        d3.csv('public/data/UEvsOTAN.csv', resolve);
+    });
+
+// retrieve datas
+Q.all([promiseGeojson, promiseData]).then(function(data){
+    console.log(data);
+});
