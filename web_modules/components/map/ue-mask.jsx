4801fe252f90a656f42c9218791b22da5d1632ba
hllwd <nicolasmondon@gmail.com>
2015-03-15 15:09:41 +0100

    - Map ; first draft for mask

---
+++ web_modules/components/map/ue-mask.jsx
@@ -0,0 +1,15 @@
+'use strict';
+
+var React = require('react');
+
+var UEMask = React.createClass({
+    render: function(){
+        return (
+            <mask id="mask-ue-map" x="0" y="0" width="1000" height="1000">
+
+            </mask>
+        )
+    }
+});
+
+module.exports = UEMask;
---
+++ web_modules/components/map/ue.jsx
@@ -0,0 +1,11 @@
+'use strict';
+
+var React = require('react');
+
+var UEMap = React.createClass({
+    render: function(){
+        return null;
+    }
+});
+
+module.exports = UEMap;
